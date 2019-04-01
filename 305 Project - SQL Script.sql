2c257c75e83796284f481511a8b42000a1da9f5a
gurpreeth956 <gurpreeth956@gmail.com>
2019-04-01 00:48:08 -0400

    Added Review table

--- 305 Project - SQL Script.sql
+++ 305 Project - SQL Script.sql
@@ -17,4 +17,41 @@ CREATE TABLE Employee(
     DateJoined DATE,
     PRIMARY KEY(EmployeeID),
     FOREIGN KEY(Supervisor) REFERENCES Employee(EmployeeID));
+                
+CREATE TABLE Item(
+	ItemID INT,
+	Price DEC(6,2),
+	ItemType VARCHAR(15),
+	Seller VARCHAR(15),
+	PRIMARY KEY(ItemID),
+	FOREIGN KEY(Inventory) REFERENCES Inventory(ItemType));
+
+CREATE TABLE Inventory(
+	Named VARCHAR(15),
+	Quantity INT,
+	Seller VARCHAR(15),
+	ItemType VARCHAR(15),
+	PRIMARY KEY(ItemType));
+
+CREATE TABLE ShoppingCart(
+	PRIMARY KEY(CustomerID, ItemID),
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
+	FOREIGN KEY(ItemID) REFERENCES Item(ItemID));
+
+CREATE TABLE WishList(
+	PRIMARY KEY(CustomerID, ItemID),
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
+	FOREIGN KEY(ItemID) REFERENCES Item(ItemID));
+                                      
+CREATE TABLE Orders(
+	OrderNum INT,
+    OrderDate DATE,
+    Completed CHAR(1),
+    PRIMARY KEY(OrderNum),
+    CHECK (Completed IN('Y', 'N')));
     
+CREATE TABLE REVIEWS(
+	Ratings INT,
+	PRIMARY KEY(OrderID),
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
+	FOREIGN KEY(OrderID) REFERENCES Orders(OrderNum));
