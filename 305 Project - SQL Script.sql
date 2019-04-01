87f9839e47a049c0b6d0623f74f64f2149927b7c
RayHuangH <rhuang502@gmail.com>
2019-03-31 23:46:45 -0400

    Item Tables
    -Created tables for items, inventory, wishlist and shopping cart

--- 305 Project - SQL Script.sql
+++ 305 Project - SQL Script.sql
@@ -5,3 +5,34 @@ CREATE TABLE Person(
     Phone VARCHAR(10),
     Address VARCHAR(30),
     PRIMARY KEY(Email));
+
+CREATE TABLE Item(
+	ItemID INT,
+	Price DEC(5,3),
+	ItemType VARCHAR(15),
+	Seller VARCHAR(15),
+	PRIMARY KEY(ItemID),
+	FOREIGN KEY(Inventory) REFERENCES Inventory(ItemType));
+
+CREATE TABLE Inventory(
+	Named VARCHAR(15),
+	Quantity INT,
+	Seller VARCHAR(15),
+	ItemType VARCHAR(15),
+	PRIMARY KEY(ItemType));
+
+CREATE TABLE ShoppingCart(
+	PRIMARY KEY(CustomerID, ItemID),
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
+	FOREIGN KEY(ItemID) REFERENCES Item(ItemID));
+
+CREATE TABLE WishList(
+	PRIMARY KEY(CustomerID, ItemID),
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
+	FOREIGN KEY(ItemID) REFERENCES Item(ItemID));
+
+CREATE TABLE Ray(
+	Id VARCHAR(20));
+
+
+
