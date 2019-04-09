57059b0a3767a9d6b4bef4e6596ef55f37c23f90
gurpreeth956 <gurpreeth956@gmail.com>
2019-04-08 20:54:49 -0400

    Added constraints for the tables

--- 305 Project - SQL Script.sql
+++ 305 Project - SQL Script.sql
@@ -1,50 +1,64 @@
 CREATE TABLE Person(
+	ID INT, #ID here is customer id since everyone will have cutomer account
 	Email VARCHAR(30),
     Named VARCHAR(25),
     Dob DATE,
     Phone VARCHAR(10),
     Address VARCHAR(30),
-    PRIMARY KEY(Email));
-                
-CREATE TABLE Item(
-	ItemID INT,
-	Price DEC(5,3),
-	ItemType VARCHAR(15),
-	Seller VARCHAR(15),
-	PRIMARY KEY(ItemID),
-	FOREIGN KEY(Inventory) REFERENCES Inventory(ItemType));
-
-CREATE TABLE Inventory(
-	Named VARCHAR(15),
-	Quantity INT,
-	Seller VARCHAR(15),
-	ItemType VARCHAR(15),
-	PRIMARY KEY(ItemType));
-
-CREATE TABLE ShoppingCart(
-	PRIMARY KEY(CustomerID, ItemID),
-	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
-	FOREIGN KEY(ItemID) REFERENCES Item(ItemID));
-
-CREATE TABLE WishList(
-	PRIMARY KEY(CustomerID, ItemID),
-	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
-	FOREIGN KEY(ItemID) REFERENCES Item(ItemID));
-	
+    DateJoined DATE,
+    IsEmployee CHAR(1),
+    PRIMARY KEY(ID),
+    CHECK (IsEmployee IN('Y', 'N')));
+    
 CREATE TABLE Customer(
-	CustomerID INT,
     Username VARCHAR(20),
     Userpass VARCHAR(20),
-    PRIMARY KEY(CustomerID));
+    PRIMARY KEY(CustomerID),
+    FOREIGN KEY(CustomerID) REFERENCES Person(ID) 
+		ON DELETE CASCADE
+        ON UPDATE CASCADE);
     
 CREATE TABLE Employee(
 	EmployeeID INT,
-    DateJoined DATE,
     PRIMARY KEY(EmployeeID),
-    FOREIGN KEY(Supervisor) REFERENCES Employee(EmployeeID));
+    FOREIGN KEY(Supervisor) REFERENCES Employee(EmployeeID) 
+		ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY(ID) REFERENCES Person(ID) 
+		ON DELETE CASCADE 
+        ON UPDATE CASCADE);
+    #need some sort of selection command
+                
+CREATE TABLE Item(
+	ItemID INT,
+    Quantity INT,
+	Price DEC(8,3),
+	ItemType VARCHAR(15),
+	Seller VARCHAR(15),
+	PRIMARY KEY(ItemID));
+
+CREATE TABLE ShoppingCart(
+	Quantity INT,
+	PRIMARY KEY(CustomerID, ItemID),
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID) 
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY(ItemID) REFERENCES Item(ItemID) 
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CHECK (Quantity < Item.Quantity));
+
+CREATE TABLE WishList(
+	PRIMARY KEY(CustomerID, ItemID),
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID) 
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY(ItemID) REFERENCES Item(ItemID) 
+		ON DELETE CASCADE
+        ON UPDATE CASCADE);
                                                 
 CREATE TABLE Payment(
-	CardNum INT,
+	CardNum INT(16),
     CVS INT(3),
     CardType VarChar(10),
     CardExp DATE,
@@ -63,6 +77,8 @@ CREATE TABLE Orders(
     OrderDate DATE,
     Completed CHAR(1),
     PRIMARY KEY(OrderNum),
+    FOREIGN KEY(CustomerID) REFERENCES Person(ID),
+    FOREIGN
     CHECK (Completed IN('Y', 'N')));
     
 CREATE TABLE REVIEWS(
@@ -70,3 +86,5 @@ CREATE TABLE REVIEWS(
 	PRIMARY KEY(OrderID),
 	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID),
 	FOREIGN KEY(OrderID) REFERENCES Orders(OrderNum));
+
+#returns???
