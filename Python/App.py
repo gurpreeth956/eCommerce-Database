1afc09ce99fe4abacca486b3850a9348d85f48d9
gurpreeth956 <gurpreeth956@gmail.com>
2019-04-29 21:05:34 -0400

    Added getters and setters for all tables in the database

--- Python/App.py
+++ Python/App.py
@@ -3,27 +3,29 @@ from flask import Flask, render_template, request
 import pymysql.cursors
 
 
-
 # do hard refresh on web page if something not loading
 app = Flask(__name__)
-
 
 
 @app.route("/")
 def home():
     return render_template('index.html', styles = 'album.css', bodyclass = 'bg-light')
 
+
 @app.route("/signup.html", methods = ['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        #NEED A UNIQUE GLOBAL NUM
         idn = 0
+
         # Connect to the database
-        client = pymysql.connect("localhost", "public", "password123", "eCommerce")
+        client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
         name = request.form['firstName']
         email = request.form['email']
         password = request.form['password']
         phone = request.form['phone']
-        print(name, email, password, phone)
+        #print(name, email, password, phone)
+
         try:
             idn += 1
             cursor = client.cursor()
@@ -32,7 +34,7 @@ def signup():
             cursor.execute(query, (idn, email, name, '1998-11-25', phone, 'Ithaca', '1998-11-25', 'N'))
             client.commit()
         except Exception:
-            print('h')
+            print(Exception)
             client.rollback()
         finally:
             client.close()
@@ -40,22 +42,27 @@ def signup():
         print("error")
     return render_template('signup.html', styles= 'signin.css', bodyclass= 'text-center')
 
+
 @app.route("/signin.html")
 def login():
     return render_template('signin.html', styles= 'signin.css', bodyclass= 'text-center')
+
 
 @app.route("/checkout.html")
 def checkout():
     return render_template('checkout.html', styles= 'form-validation.css', bodyclass= 'bg-light')
 
+
 @app.route("/shop.html")
 def shop():
     return render_template('shop.html', styles= '', bodyclass= 'bg-light')
+
 
 @app.route("/item.html")
 def item():
     return render_template('item.html', styles= '', bodyclass= 'bg-light')
 
+
 # to run in python
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
--- Python/dbBasics.py
+++ Python/dbBasics.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 def insert():
-    client = pymysql.connect("localhost", "public", "password123", "eCommerce")
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
     try:
         cursor = client.cursor()
         query = "INSERT INTO Person(ID, Email, Named, DateOfBirth, Phone, Address, DateJoined, IsEmployee)\
@@ -21,7 +21,8 @@ def select():
     client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
     try:
         cursor = client.cursor()
-        query = "SELECT ID, Email, Named, DateOfBirth, Phone, Address, DateJoined, IsEmployee FROM Person"
+        query = "SELECT ID, Email, Named, DateOfBirth, Phone, Address, DateJoined, IsEmployee FROM Person \
+                WHERE ID = 251059"
         cursor.execute(query)
         results = cursor.fetchall()
         for row in results:
@@ -33,5 +34,398 @@ def select():
 
 
 # To test methods:
-insert()
+# insert()
 # select()
+
+
+'''
+BELOW ARE ALL THE METHODS FOR GETTING AND SETTING FROM THE DATABASE.
+
+NOTE: THIS FILE IS TEMPORARY AND THE METHODS WILL BE MOVED TO THE NECESSARY FILE(S) WHEN THEY ARE CREATED
+'''
+
+
+# Person Table
+def insertPerson(idvar, email, name, birthdate, phone, address, datejoined, isemployee, username, userpass,
+                 hasmembership, employeeid, supervisor):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Person(ID, Email, Named, DateOfBirth, Phone, Address, DateJoined, IsEmployee)\
+            values(%s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (idvar, email, name, birthdate, phone, address, datejoined, isemployee))
+        client.commit()
+
+        if isEmployeeVar == 'N':
+            insertCustomer(idVar, username, userpass, hasmembership)
+        elif isEmployeeVar == 'Y':
+            insertEmployee(idVar, employeeid, supervisor)
+    except Exception:
+        print("Could not add entity to Person table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getPerson(idvar):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT ID, Email, Named, DateOfBirth, Phone, Address, DateJoined, IsEmployee FROM Person \
+            WHERE ID = %s"
+        cursor.execute(query, idvar)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Person Entity")
+    finally:
+        client.close()
+
+
+# Customer Table
+def insertCustomer(idvar, username, userpass, hasmembership):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Customer(ID, Username, Userpass, HasMembership) values(%s, %s, %s, %s)"
+        cursor.execute(query, (idvar, username, userpass, hasmembership))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Customer table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getCustomer(idvar):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT ID, Username, Userpass, HasMembership FROM Customer WHERE ID = %s"
+        cursor.execute(query, idvar)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Customer Entity")
+    finally:
+        client.close()
+
+
+# Employee Table
+def insertEmployee(idvar, employeeid, supervisor):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Employee(ID, EmployeeID, Supervisor) values(%s, %s, %s)"
+        cursor.execute(query, (idvar, employeeid, supervisor))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Employee table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getEmployee(employeeid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT ID, EmployeeID, Supervisor FROM Employee WHERE EmployeeID = %s"
+        cursor.execute(query, employeeid)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Employee Entity")
+    finally:
+        client.close()
+
+
+# Item Table
+def insertItem(itemid, quantity, price, itemtype, seller, itemdesc):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Item(ItemID, Quantity, Price, ItemType, Seller, ItemDesc) values(%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (itemid, quantity, price, itemtype, seller, itemdesc))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Item table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getItem(itemid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT ItemID, Quantity, Price, ItemType, Seller, ItemDesc FROM Item WHERE ItemID = %s"
+        cursor.execute(query, itemid)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Item Entity")
+    finally:
+        client.close()
+
+
+# ShoppingCart Table
+def insertShoppingCart(customerid, itemid, quantity):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO ShoppingCart(CustomerID, ItemID, Quantity) values(%s, %s, %s)"
+        cursor.execute(query, (customerid, itemid, quantity))
+        client.commit()
+    except Exception:
+        print("Could not add entity to ShoppingCart table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getShoppingCart(customerid, itemid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT CustomerID, ItemID, Quantity FROM Customer WHERE CustomerID = %s AND ItemID = %s"
+        cursor.execute(query, (customerid, itemid))
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified ShoppingCart Entity")
+    finally:
+        client.close()
+
+
+# WishList Table
+def insertWishList(customerid, itemid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO WishList(CustomerID, ItemID) values(%s, %s, %s)"
+        cursor.execute(query, (customerid, itemid))
+        client.commit()
+    except Exception:
+        print("Could not add entity to WishList table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getWishList(customerid, itemid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT CustomerID, ItemID FROM WishList WHERE CustomerID = %s AND ItemID = %s"
+        cursor.execute(query, (customerid, itemid))
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified WishList Entity")
+    finally:
+        client.close()
+
+
+# Discount Table
+def insertDiscount(discountid, discountprice, excdate):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Discount(DiscountID, DiscountPrice, ExpDate) values(%s, %s, %s)"
+        cursor.execute(query, (discountid, discountprice, excdate))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Discount table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getDiscount(discountid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT DiscountID, DiscountPrice, ExpDate FROM Discount WHERE DiscountID = %s"
+        cursor.execute(query, discountid)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Discount Entity")
+    finally:
+        client.close()
+
+
+# Orders Table
+def insertOrder(orderid, customerid, orderdate, completed, discountid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Item(OrderNum, CustomerID, OrderDate, Completed, DiscountID) values(%s, %s, %s, %s, %s)"
+        cursor.execute(query, (orderid, customerid, orderdate, completed, discountid))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Orders table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getOrder(orderid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT OrderNum, CustomerID, OrderDate, Completed, DiscountID FROM Orders WHERE OrderNum = %s"
+        cursor.execute(query, orderid)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Orders Entity")
+    finally:
+        client.close()
+
+
+# OrderedItems Table
+def insertOrderedItems(orderid, itemid, quantity):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO OrderedItems(OrderID, ItemID, Quantity) values(%s, %s, %s)"
+        cursor.execute(query, (orderid, itemid, quantity))
+        client.commit()
+    except Exception:
+        print("Could not add entity to OrderedItems table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getOrderedItems(orderid, itemid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT OrderID, ItemID, Quantity FROM OrderedItems WHERE OrderID = %s AND ItemID = %s"
+        cursor.execute(query, (orderid, itemid))
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified OrderedItems Entity")
+    finally:
+        client.close()
+
+
+# Payment Table
+def insertPayment(orderid, cardnum, cvs, cardcomp, cardtype, cardexp):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Payment(OrderID, CardNum, CVS, CardComp, CardType, CardExp) values(%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (orderid, cardnum, cvs, cardcomp, cardtype, cardexp))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Payment table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getPayments(orderid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT OrderID, CardNum, CVS, CardComp, CardType, CardExp FROM Payment WHERE OrderID = %s"
+        cursor.execute(query, orderid)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Payment Entity")
+    finally:
+        client.close()
+
+
+# Shipment Table
+def insertShipment(orderid, address, details, fee, company):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Shipment(OrderID, Address, Details, Fee, Company) values(%s, %s, %s, %s, %s)"
+        cursor.execute(query, (orderid, address, details, fee, company))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Shipment table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getShipment(orderid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT OrderID, Address, Details, Fee, Company FROM Shipment WHERE OrderID = %s"
+        cursor.execute(query, orderid)
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Shipment Entity")
+    finally:
+        client.close()
+
+
+# Returnment Table
+def insertReturnment(orderid, itemid, quantity, comments):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Returnment(OrderID, ItemID, Quantity, Comments) values(%s, %s, %s, %s)"
+        cursor.execute(query, (orderid, itemid, quantity, comments))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Returnment table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getReturnment(orderid, itemid, quantity):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT OrderID, ItemID, Quantity, Comments FROM Returnment WHERE OrderID = %s AND ItemID = %s \
+            AND Quantity = %s"
+        cursor.execute(query, (orderid, itemid, quantity))
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Returnment Entity")
+    finally:
+        client.close()
+
+
+# Reviews Table
+def insertReviews(customerid, itemid, ratings, comments):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "INSERT INTO Reviews(CustomerID, ItemID, Ratings, Comments) values(%s, %s, %s, %s)"
+        cursor.execute(query, (customerid, itemid, ratings, comments))
+        client.commit()
+    except Exception:
+        print("Could not add entity to Reviews table")
+        client.rollback()
+    finally:
+        client.close()
+
+
+def getReviews(customerid, itemid):
+    client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+    try:
+        cursor = client.cursor()
+        query = "SELECT CustomerID, ItemID, Ratings, Comments FROM Reviews WHERE CusotmerID = %s AND ItemID = %s"
+        cursor.execute(query, (customerid, itemid))
+        result = cursor.fetchall()
+        return result
+    except Exception:
+        print("Can not retrieve specified Reviews Entity")
+    finally:
+        client.close()
