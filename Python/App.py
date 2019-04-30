dafda6e48869421ef6e13418e093609b78e2c933
Tenzin Loden <tltloden7@gmail.com>
2019-04-29 23:49:51 -0400

    User Login

--- Python/App.py
+++ Python/App.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect
 import pymysql.cursors
 
 
 # do hard refresh on web page if something not loading
 app = Flask(__name__)
 
+loggedin = None
 
 @app.route("/")
 def home():
-    return render_template('index.html', styles = 'album.css', bodyclass = 'bg-light')
+    global loggedin
+    print(loggedin)
+    return render_template('index.html', loggedin= loggedin, title= 'Home', styles = 'album.css', bodyclass = 'bg-light')
 
 
 @app.route("/signup.html", methods = ['GET', 'POST'])
@@ -17,7 +19,6 @@ def signup():
     if request.method == 'POST':
         #NEED A UNIQUE GLOBAL NUM
         idn = 0
-
         # Connect to the database
         client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
         name = request.form['firstName']
@@ -40,17 +41,38 @@ def signup():
             client.close()
     else:
         print("error")
-    return render_template('signup.html', styles= 'signin.css', bodyclass= 'text-center')
+    return render_template('signup.html', title= 'Sign Up',  styles= 'signin.css', bodyclass= 'text-center')
 
 
-@app.route("/signin.html")
+@app.route("/signin.html", methods= ['GET', 'POST'])
 def login():
-    return render_template('signin.html', styles= 'signin.css', bodyclass= 'text-center')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        client = pymysql.connect("localhost", "public", "password123", "eCommerce01")
+        try:
+            cursor = client.cursor()
+            query = "SELECT P.Email, C.Userpass, C.Username, C.CustomerID, P.ID FROM Customer C, Person P WHERE C.CustomerID = P.ID"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            for customer in result:
+                if customer[0] == email and customer[1] == password:
+                    global loggedin
+                    loggedin = customer[2]
+                    print('Logged In')
+                    break
+            if loggedin != None:
+                return redirect('/')
+        except Exception:
+            print("Can not retrieve specified Customer Entity")
+        finally:
+            client.close()
+    return render_template('signin.html', title= 'Log In', styles= 'signin.css', bodyclass= 'text-center')
 
 
 @app.route("/checkout.html")
 def checkout():
-    return render_template('checkout.html', styles= 'form-validation.css', bodyclass= 'bg-light')
+    return render_template('checkout.html', title= 'Shopping Cart', styles= 'form-validation.css', bodyclass= 'bg-light')
 
 
 @app.route("/shop.html")
@@ -99,12 +121,12 @@ def shop():
             'price': price
         }
     ]
-    return render_template('shop.html', data=data, styles='', bodyclass='bg-light')
+    return render_template('shop.html', title= 'Shop', data=data, styles='', bodyclass='bg-light')
 
 
 @app.route("/item.html")
 def item():
-    return render_template('item.html', styles= '', bodyclass= 'bg-light')
+    return render_template('item.html', title= '[Item Name]', styles= '', bodyclass= 'bg-light')
 
 
 # to run in python
