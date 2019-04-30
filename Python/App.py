5950ce2582d9f9ccc63eb0b77c5dfadec4cc527b
RayHuangH <rhuang502@gmail.com>
2019-04-30 01:34:58 -0400

    Order History and WishList
    -Created order history and wishlist html pages

--- Python/App.py
+++ Python/App.py
@@ -5,7 +5,7 @@ import pymysql.cursors
 # do hard refresh on web page if something not loading
 app = Flask(__name__)
 
-loggedin = None
+loggedin = "Sfasd"
 
 @app.route("/")
 def home():
@@ -145,6 +145,14 @@ def item():
 @app.route("/profile.html")
 def profile():
     return render_template('profile.html', title='Profile', styles='', bodyclass='bg-light')
+
+@app.route("/history.html")
+def history():
+    return render_template('history.html', title='Order History', styles='history.css', bodyclass='bg-light')
+
+@app.route("/wishlist.html")
+def wishlist():
+    return render_template('wishlist.html', title='Wish List', styles='wishlist.css', bodyclass='bg-light')
 
 # to run in python
 if __name__ == '__main__':
